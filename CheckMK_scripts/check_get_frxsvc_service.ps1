40ed4755b16a5e154c9abd40b1d852d70808ff64
Damian <scherlda@outlook.com>
2022-01-11 15:18:10 +0100

    CheckMK_scripts

---
+++ CheckMK_scripts/check_get_frxsvc_service.ps1
@@ -0,0 +1,13 @@
+﻿$serviceStatus = Get-Service frxsvc
+$date = Get-Date -Format "yyyyddMM_HHmm"
+if ($serviceStatus.Status -eq 'Running')
+{
+# If Service is Running, tactus Service = OK!
+    Write-Host "0 ps1_get_frxsvc_service - FsLogix Dienst OK"
+}
+else
+{
+# If Service is not Running. Report sms via Tactus. Create File with, Users in Filename YearMonthDay_HourMinutes
+    Write-Host "2 ps1_get_frxsvc_service - FsLogix Dienst NICHT OK"
+    query user >> c:\sci\CrashedUsers_$date.txt
+}
---
+++ CheckMK_scripts/check_items_in_folder.ps1
@@ -0,0 +1,35 @@
+﻿###################################################
+# Check MK Script - check if files left in folder #
+###################################################
+# Made by: Damian Scherl                          #
+# Made at: 27.12.2021                             #
+###################################################
+#            Time Trigger via Check MK            #
+# The Scripts retuns if there is a File in the    #
+# mypath variable Path (Folder)                   #
+###################################################
+# Last change by: Damian Scherl                   #
+# Last change at: 10.01.2022                      #
+###################################################
+
+# User editable variables
+$mypath = 'C:\EEAttachments'
+$filetype = '*.pdf'
+$timeInMinutesToGoBack = 30
+
+#Calculatet variables
+#Date - USer Definiet Minutes
+$filedate = (Get-Date).AddMinutes(-$timeInMinutesToGoBack)
+$fileItemCount = (Get-ChildItem -Path $mypath $filetype | Where-Object {$_.LastWriteTime -lt $filedate}).Count
+
+#return to checkmk
+#If the File count is greater than (-gt), check is not OK, else check is OK
+if($fileItemCount -gt 0)
+{
+    Write-Host "2 check_items_in_folder - PDFs left => NICHT OK"
+}
+else
+{
+    Write-Host "0 check_items_in_folder - No PDFs left => OK"
+}
+
