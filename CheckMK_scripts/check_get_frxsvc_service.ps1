19e58cacbe5b8f509047e931fa50469582f72c30
Damian <scherlda@outlook.com>
2024-08-23 17:29:08 +0200

    Update check_get_frxsvc_service.ps1

--- CheckMK_scripts/check_get_frxsvc_service.ps1
+++ CheckMK_scripts/check_get_frxsvc_service.ps1
@@ -1,13 +1,20 @@
-﻿$serviceStatus = Get-Service frxsvc
-$date = Get-Date -Format "yyyyddMM_HHmm"
+# General variables for paths and filenames
+$serviceName = "frxsvc"
+$outputDirectory = "C:\sci"
+$dateFormat = "yyyyddMM_HHmm"
+$date = Get-Date -Format $dateFormat
+
+# Check the status of the service
+$serviceStatus = Get-Service $serviceName
+
 if ($serviceStatus.Status -eq 'Running')
 {
-# If Service is Running, tactus Service = OK!
-    Write-Host "0 ps1_get_frxsvc_service - FsLogix Service OK"
+    # If the service is running, report OK!
+    Write-Host "0 ps1_get_${serviceName}_service - $serviceName Service OK"
 }
 else
 {
-# If Service is not Running. Report sms via Tactus. Create File with, Users in Filename YearMonthDay_HourMinutes
-    Write-Host "2 ps1_get_frxsvc_service - FsLogix Service NOT OK"
-    query user >> c:\sci\CrashedUsers_$date.txt
+    # If the service is not running, report NOT OK and create a file with the current date and time
+    Write-Host "2 ps1_get_${serviceName}_service - $serviceName Service NOT OK"
+    query user >> "$outputDirectory\CrashedUsers_$date.txt"
 }
